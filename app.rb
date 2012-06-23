429d0215ddbfa29df448f696afe5acba1cdac24f
Titus Ramczykowski <ramczykowski@adrodev.de>
2012-06-23 21:29:29 +0200

    heroku test2

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 
-get '/' do
-  dir = Dir.open("pictures")
-  dir.inspect
+class App < Sinatra::Base
+#   register Mustache::Sinatra
+#   require 'views/layout'
+
+  set :mustache, {
+    :views     => 'views/',
+    :templates => 'templates/'
+  }
+
+  get '/' do
+    @title = "Mustache + Sinatra = Wonder"
+    dir = Dir.open("pictures")
+    dir.inspect
+  end
+
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 require 'app'
 
+$stdout.sync = true
+
 use Rack::ShowExceptions
 
 run App.new
